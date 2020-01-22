6635f6f6137e7fc14039cdc013fb050d5dbace2f
Andre Freeman <asf@mail.usf.edu>
2020-01-21 22:20:07 -0500

    complete Baby Task

--- index.js
+++ index.js
@@ -88,10 +88,16 @@ Car.prototype.fill = function(gallons) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
 
+  Person.call(this,name,age);
+  this.favoriteToy = favoriteToy;
+ 
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(favoriteToy){
+  return `Playing with ${this.favoriteToy}`;
+}
 /* 
   TASK 4
 
