9c3bf6a3445722a93c11edcf93ebea31cec1e722
Andre Freeman <asf@mail.usf.edu>
2020-01-23 20:17:31 -0500

    Part 1 stretch completed

--- index.js
+++ index.js
@@ -80,6 +80,16 @@ this.odometer = 0;
 Car.prototype.fill = function(gallons) {
   return this.tank += gallons;
 }
+Car.prototype.drive = function(distance) {
+  this.odometer += distance;
+  this.tank -= (distance/this.milesPerGallon);
+  if(this.tank <= 0) {
+
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+  
+}
+
 
 /*
   TASK 3
